c9178358df35194195b48c2fcc3effd09260dbb9
mattpaletta <mattpaletta@gmail.com>
2017-03-16 12:12:42 -0700

    Uploaded Login Page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import {Page1} from '../page1/page1';
+
+
+import { NavController, NavParams, ModalController, ToastController, ViewController } from 'ionic-angular';
+
+@Component({
+	selector: 'page-login',
+	templateUrl: 'login.html'
+})
+export class Login {
+	username = "";
+	password = "";
+
+constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController, public toastCtrl: ToastController, public viewCtrl: ViewController) {
+	this.username = "";
+	this.password = "";
+}
+
+loginUser() {
+	if (this.username == "") {
+		return this.toastCtrl.create({
+			message: "Enter A Username",
+			duration: 1500
+		}).present();
+	}
+	
+	if (this.password == "") {
+		return this.toastCtrl.create({
+			message: "Enter A Username",
+			duration: 1500
+		}).present();
+	}
+	
+	const index = this.viewCtrl.index;
+    // then we remove it from the navigation stack
+    this.navCtrl.popToRoot();
+	this.navCtrl.push(Page1);
+}
+}
+
